5eec3af4e537f68889755dff6f352a53ee09b54f
Renan <renanlimabl@gmail.com>
2019-12-24 15:09:56 -0300

    Adicina module exports for node support

--- groupArray/index.js
+++ groupArray/index.js
@@ -1,4 +1,5 @@
-export default function chunkArray(myArray, chunk_size){
+'use strict'
+module.exports = function chunkArray(myArray, chunk_size){
     var tempArray = [];    
     for (var i = 0; i < myArray.length; i += chunk_size) {
         myChunk = myArray.slice(i, i+chunk_size);        

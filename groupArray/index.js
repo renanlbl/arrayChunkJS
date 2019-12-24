2b3daba6017fd707ba1ace84c9226fd13a342873
Renan <renanlimabl@gmail.com>
2019-12-24 13:50:36 -0300

    Adiciona primeiro commit

---
+++ groupArray/index.js
@@ -0,0 +1,8 @@
+export default function chunkArray(myArray, chunk_size){
+    var tempArray = [];    
+    for (var i = 0; i < myArray.length; i += chunk_size) {
+        myChunk = myArray.slice(i, i+chunk_size);        
+        tempArray.push(myChunk);
+    }
+    return tempArray;
+}
